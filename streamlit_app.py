ed9c01e9bf383206a2f210e2dea5b083cd1f6742
thouseplp <100323497+thouseplp@users.noreply.github.com>
2024-10-09 03:37:46 +0000

    All

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ test = ['Test', 'Cat']
 with st.sidebar:
     st.header("Test")
     for i in test:
-        st.markdown(f"<div style='padding-left: 20px;'>{i}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div style='padding-left: 20px; font-size: 35px'>{i}</div>", unsafe_allow_html=True)
