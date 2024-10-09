e2bc4ebcfd11d83a0bb6db73a362730cb329280d
thouseplp <100323497+thouseplp@users.noreply.github.com>
2024-10-09 03:36:55 +0000

    All

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,8 @@
 import streamlit as st
 
-# Inject custom CSS to remove the border around st.expander
-hide_expander_border = """
-    <style>
-    [data-testid="stExpander"] details {
-        border: none !important;
-    }
-    </style>
-"""
+test = ['Test', 'Cat']
 
-# Inject the CSS into the app
-st.markdown(hide_expander_border, unsafe_allow_html=True)
-
-custom_css = """
-    <style>
-    [data-testid="stExpander"] details summary {
-        font-size: 20rem !important;
-    }
-    </style>
-"""
-
-# Inject the CSS into the app
-st.markdown(custom_css, unsafe_allow_html=True)
-
-# Example of expander with larger label text
 with st.sidebar:
-    with st.expander("Larger Label Text", expanded=True):
-        st.write("This expander label is larger.")
+    st.header("Test")
+    for i in test:
+        st.markdown(f"<div style='padding-left: 20px;'>{i}</div>", unsafe_allow_html=True)
